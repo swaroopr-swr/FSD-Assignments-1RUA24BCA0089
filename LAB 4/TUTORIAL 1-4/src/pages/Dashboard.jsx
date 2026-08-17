ee2a6e6aa2b5cbb38fe07b031d122c8e9fdfdb60
Swaroop <swarp82@gmail.com>
2026-08-17 12:25:32 +0530

    Fix unused import for Vercel deployment

--- LAB 4/TUTORIAL 1-4/src/pages/Dashboard.jsx
+++ LAB 4/TUTORIAL 1-4/src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Card from '../components/Card';
+
 import Button from '../components/Button';
 import PracticalSummary from '../sections/PracticalSummary';
 
